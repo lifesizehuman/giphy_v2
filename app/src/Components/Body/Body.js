e73c05be5438ebd3e29a5bf0735682713effdf0a
Slater Combes <combes.slater@gmail.com>
2018-01-31 12:51:05 -0800

    create simple file structure

---
+++ app/src/Components/Body/Body.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Body.css'
+
+class Body extends React.Component {
+  render() {
+    return (
+      <section>
+        <h1>Hello World!</h1>
+      </section>
+    )
+  }
+}
+
+export default Body
