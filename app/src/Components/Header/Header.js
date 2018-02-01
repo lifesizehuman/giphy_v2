bf66be06322c4442744a9d87071d5423450196c9
Slater Combes <combes.slater@gmail.com>
2018-01-31 19:36:32 -0800

    changes

--- app/src/Components/Header/Header.js
+++ app/src/Components/Header/Header.js
@@ -1,41 +1,39 @@
 import React from 'react'
 import './Header.css'
 
-class Header extends React.Component {
+const queryURL = "https://api.giphy.com/v1/gifs/search?q=/"
+let searchQuery = document.getElementById("searchBox")
+const apiKey = "&api_key=dc6zaTOxFJmzC"
 
-  searchGifs = () => {
-    console.log("this is working")
-    const queryURL = "https://api.giphy.com/v1/gifs/search?q=/"
-    let searchQuery = document.getElementById("searchBox")
-    const apiKey = "&api_key=dc6zaTOxFJmzC"
+const searchGifs = function() {
+  console.log("this is working")
 
-    if (!searchQuery) {
-      searchQuery = "matt%20damon"
-    }
-    let images = [];
-    let results = document.getElementById('results')
-
-    fetch(queryURL + searchQuery + apiKey).then(res => res.json()).then((res) => {
-      let searchResults = document.createElement("<p>");
-      console.log("this is working too")
-      console.log(res);
-      for (var i = 0; i < res.data.length; i++) {
-        let gif = res.data[i].images.downsized.url
-        images.push(gif);
-      }
-
-      console.log(images);
-
-      for (let j = 0; j < images.length; j++) {
-        searchResults.append("<img src='" + images[i].src + "' />")
-        results.append(searchResults)
-      }
-      console.log("this is also working")
-    }, (error) => {
-      console.log(error);
-    })
+  if (!searchQuery) {
+    searchQuery = "matt%20damon"
   }
+  let gifs = [];
+  let results = document.getElementById('results')
 
+  fetch(queryURL + searchQuery + apiKey).then(res => res.json()).then((res) => {
+    console.log("this is working too")
+    console.log(res);
+    for (var i = 0; i < res.data.length; i++) {
+      let gif = res.data[i].images.downsized.url
+      gifs.push(gif);
+    }
+  }).then(() => {
+    console.log(gifs);
+    for (var j = 0; j < gifs.length; j++) {
+      results.append("<img src='" + gifs[j] + "' />")
+    }
+    console.log(results)
+    console.log("this is also working")
+  }, (error) => {
+    console.log(error);
+  })
+}
+
+class Header extends React.Component {
   render() {
     return (<section>
       <div className='jumbotron'>
@@ -45,10 +43,10 @@ class Header extends React.Component {
           <div className="form-group">
             <input className="form-control form-control-lg" id="searchBox" type="text" placeholder="Search and Press Enter"/>
             <br/>
-            <button type="button" onClick={this.searchGifs()} id="searchBtn" className="btn btn-dark">Search</button>
+            <button type="button" onClick={searchGifs} id="searchBtn" className="btn btn-dark">Search</button>
           </div>
         </div>
-        <div id="results"/>
+        <div id="results"></div>
       </div>
     </section>)
   }
