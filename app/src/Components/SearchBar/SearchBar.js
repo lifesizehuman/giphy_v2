42c4b07663c466a8cdb525c250ecc41944db4b5a
Slater Combes <combes.slater@gmail.com>
2018-02-01 14:33:51 -0800

    changes

--- app/src/Components/SearchBar/SearchBar.js
+++ app/src/Components/SearchBar/SearchBar.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import ReactLogo from "./React.png"
+import GiphyLogo from "./giphy.png"
 
 class SearchBar extends React.Component {
   constructor() {
@@ -16,6 +18,8 @@ class SearchBar extends React.Component {
   render() {
     return (<div className="jumbotron">
       <div className='vertical-center'>
+        <div id="rlogo" style={{ display: "inline-block" }}><img src={ReactLogo} /></div>
+        <div id="glogo" style={{ display: "inline-block" }}><img src={GiphyLogo} /></div>
         <h1>React Giphy Search Tool</h1>
         <h2>by Slater Combes</h2>
         <div className="search">
